0e667c9d956ca52afb3f82f0782cc53b54af6dd2
amber-ensley <amber.ensley@quotumtech.com>
2022-02-01 18:48:06 -0500

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>March 30 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
